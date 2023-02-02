27e82904313b9968e05a24c71ac68ade2c48c79a
VanDeSurk <123731891+VanDeSurk@users.noreply.github.com>
2023-02-02 17:52:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def insert_row_snowflake(new_fruit):
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')+
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
